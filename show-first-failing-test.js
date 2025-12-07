35632fb3554484c26602c2e77ea7c6f9aedf3d0e
Brandon Southwick <southwick.brandon21@gmail.com>
2025-12-07 00:14:26 -0700

    Massive cleanup

--- show-first-failing-test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const tests = JSON.parse(fs.readFileSync('feature_list.json', 'utf8'));
-const test = tests.find(t => !t.passes);
-console.log(JSON.stringify(test, null, 2));
